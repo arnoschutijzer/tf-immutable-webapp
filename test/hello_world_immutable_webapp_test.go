61cfedbbc0b3f6dbb8af50b3dfd6613e10032981
Arno Schutijzer <arno.schutijzer@gmail.com>
2020-10-01 21:41:06 +0200

    test: validate the website endpoint response

--- test/hello_world_immutable_webapp_test.go
+++ test/hello_world_immutable_webapp_test.go
@@ -3,12 +3,14 @@ package test
 import (
 	"fmt"
 	"os"
+	"strings"
 	"testing"
 	"time"
 
 	awssdk "github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/gruntwork-io/terratest/modules/aws"
+	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 )
@@ -40,9 +42,22 @@ func TestTerraformHelloWorldExample(t *testing.T) {
 	s3uri := terraform.Output(t, terraformOptions, "s3_uri")
 	expectedS3Uri := fmt.Sprintf("s3://%s", bucketName)
 
+	websiteEndpoint := fmt.Sprintf(
+		"http://%s",
+		terraform.Output(t, terraformOptions, "s3_website_endpoint"),
+	)
+
+	http_helper.HttpGetWithCustomValidation(t, websiteEndpoint, nil, ValidateResponse)
 	assert.Equal(t, s3uri, expectedS3Uri)
 
 	defer CleanUpState(t, awsRegion, bucketName, terraformOptions)
+}
+
+func ValidateResponse(status int, body string) bool {
+	// TODO: check if the environment variables are rendered correctly.
+	isValidStatus := status == 200
+	isBodyValid := strings.Contains(body, "current env variables")
+	return isValidStatus && isBodyValid
 }
 
 func DeployApplication(t *testing.T, awsRegion string, bucketName string, fileName string, directory string) {
